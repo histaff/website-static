a91e0eab130c22438a5822ff7083190fab8b334e
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-05-29 20:20:07 +0430

    Update website title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + WordPress" />
+    <Helmet title="HiStaff" />
     <Navbar />
     <div>{children}</div>
   </div>
