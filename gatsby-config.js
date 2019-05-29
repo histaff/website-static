17c747ac7fc352f3871ed71266c63cdb861a8e46
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-05-29 21:57:05 +0430

    Build production

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'soufi.me',
+        baseUrl: 'wp.histaff.io',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
