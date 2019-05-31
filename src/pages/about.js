18116a2fa439036d9b9d6426e2060223deeb3fc9
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-05-31 17:43:52 +0430

    Add about and contact page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const AboutPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="About" />
+    <h1>Hi from the about page</h1>
+    <p>Welcome to about</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,15 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Make Happy your Workspace and Employers!" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about/">About</Link>
+    <Link to="/contact/">Contact</Link>
   </Layout>
 )
 
