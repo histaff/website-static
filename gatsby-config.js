f647a3cd046d20035991e568f7faefa70d9071d1
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-05-31 17:55:16 +0430

    Update gavicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,9 +22,9 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#FF54AC`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/histaff.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
