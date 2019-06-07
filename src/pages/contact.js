471828ad1e99d267ddef0a3f240b66aaef304537
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-06-07 17:41:50 +0430

    Complete the privacy and terms page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,17 @@ const ContactPage = () => (
     <Layout>
         <SEO title="Contact"/>
         <div className={"container"}>
-            <p>Contact content</p>
+            <div className={"content"}>
+                <div className={"title"}>
+                    <h1>Contact</h1>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing
+                    elit. Aenean commodo ligula eget dolor. Aenean massa.
+                    Cum sociis natoque penatibus et magnis dis parturient
+                    montes, nascetur ridiculus mus. Donec quam felis,
+                    ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            </div>
         </div>
     </Layout>
 )
