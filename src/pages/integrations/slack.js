24447ee491fca05f00dbbd3ce2acfb9a608046a5
Mostafa Soufi <mostafa.soufi@hotmail.com>
2020-10-17 11:27:11 +0300

    Missed alt attribute for an image on slack.js #12

--- src/pages/integrations/slack.js
+++ src/pages/integrations/slack.js
@@ -14,7 +14,7 @@ const AboutPage = () => (
         <SEO title="Slack Integration"/>
 
         <div className={"page-header"}>
-            <img src={SlackHiStaffImage}/>
+            <img alt={"Slack"} src={SlackHiStaffImage}/>
             <h2>HiStaff and Slack</h2>
             <p>HiStaff is excited to announce a new integration with Slack<br />if you use the Slack it can be really efficient for your team.</p>
         </div>
